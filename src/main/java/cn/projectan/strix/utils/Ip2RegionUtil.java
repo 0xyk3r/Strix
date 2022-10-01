7ffa0ce1e54fe2a521778c205f5f02079118d7c5
0xyk3r <anjiongyi@163.com>
2022-10-01 19:24:03 +0800

    修复resources目录相关问题

--- src/main/java/cn/projectan/strix/utils/Ip2RegionUtil.java
+++ src/main/java/cn/projectan/strix/utils/Ip2RegionUtil.java
@@ -1,5 +1,6 @@
 package cn.projectan.strix.utils;
 
+import cn.hutool.core.io.file.FileWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.lionsoul.ip2region.xdb.Searcher;
 import org.springframework.core.io.ClassPathResource;
@@ -7,6 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.file.Files;
 
 /**
  * @author 安炯奕
@@ -21,7 +23,9 @@ public class Ip2RegionUtil {
         ClassPathResource resource = new ClassPathResource("ip2region/ip2region.xdb");
         File file = null;
         try {
-            file = resource.getFile();
+            file = Files.createTempFile("ip2region", ".xdb").toFile();
+            FileWriter writer = new FileWriter(file);
+            writer.writeFromStream(resource.getInputStream(), true);
         } catch (IOException e) {
             log.error("Strix IP-Region: 数据库文件读取失败", e);
         }
