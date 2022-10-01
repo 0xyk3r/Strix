f3661d87b4e62d4df2aabaaff01cc5300266d8b3
0xyk3r <anjiongyi@163.com>
2022-10-01 19:32:11 +0800

    debug

--- src/main/java/cn/projectan/strix/utils/Ip2RegionUtil.java
+++ src/main/java/cn/projectan/strix/utils/Ip2RegionUtil.java
@@ -29,17 +29,19 @@ public class Ip2RegionUtil {
         } catch (IOException e) {
             log.error("Strix IP-Region: 数据库文件读取失败", e);
         }
-        byte[] cBuff;
-        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-            cBuff = Searcher.loadContent(raf);
-            searcher = Searcher.newWithBuffer(cBuff);
-            log.info("Strix IP-Region: 初始化成功");
-        } catch (Exception e) {
-            log.error("Strix IP-Region: 初始化IP-Region功能失败", e);
+        if (file != null) {
+            byte[] cBuff;
+            try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+                cBuff = Searcher.loadContent(raf);
+                searcher = Searcher.newWithBuffer(cBuff);
+                log.info("Strix IP-Region: 初始化成功");
+            } catch (Exception e) {
+                log.error("Strix IP-Region: 初始化IP-Region功能失败", e);
+            }
         }
     }
 
-    public static String getRegion(String ip) {
+    public static String convert(String ip) {
         if (searcher == null) {
             throw new IllegalArgumentException("Strix IP-Region: 功能未初始化");
         }
@@ -50,15 +52,6 @@ public class Ip2RegionUtil {
             log.error("Strix IP-Region: 获取IP-Region失败", e);
             return "unknown";
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        System.out.println(getRegion("1.1.1.1"));
-        System.out.println(getRegion("114.114.114.114"));
-        System.out.println(getRegion("119.29.29.29"));
-        System.out.println(getRegion("223.5.5.5"));
-        System.out.println(getRegion("8.8.8.8"));
-        System.out.println(getRegion("114.244.69.97"));
     }
 
 }
