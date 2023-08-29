fa0554f099475377ff7674007a8e85ff47aa97de
0xyk3r <anjiongyi@163.com>
2023-08-29 16:06:37 +0800

    Strix 1.5.3
    Feats
    1. 增加 Common Api `getImage`.
    
    Fixes
    无.

---
+++ src/main/java/cn/projectan/strix/controller/api/common/FileController.java
@@ -0,0 +1,33 @@
+package cn.projectan.strix.controller.api.common;
+
+import cn.projectan.strix.controller.api.base.BaseApiController;
+import cn.projectan.strix.model.annotation.Anonymous;
+import cn.projectan.strix.service.OssFileService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 安炯奕
+ * @date 2023/5/26 21:57
+ */
+@Slf4j
+@RestController("ApiCommonFileController")
+@RequestMapping("api/common/file")
+@RequiredArgsConstructor
+public class FileController extends BaseApiController {
+
+    private final OssFileService ossFileService;
+
+    @Anonymous
+    @GetMapping("{fileId}")
+    public void getImage(@PathVariable String fileId, HttpServletResponse response) throws Exception {
+        response.setContentType("image/jpeg");
+        response.sendRedirect(ossFileService.getUrl(fileId, "https://oss.huiboche.cn/System/404.png"));
+    }
+
+}
