2e523c18f3bf89834695cd1e328d84d2aea4ffcf
0xyk3r <anjiongyi@163.com>
2023-09-05 17:40:11 +0800

    Strix 1.5.5
    Feats
    1. add ext to mimetype util.
    2. `getFile` api auto content type.
    
    Fixes
    empty.

--- src/main/java/cn/projectan/strix/utils/FileExtUtil.java
+++ src/main/java/cn/projectan/strix/utils/FileExtUtil.java
@@ -3,6 +3,10 @@ package cn.projectan.strix.utils;
 import cn.projectan.strix.core.exception.StrixException;
 import org.apache.tika.mime.MimeType;
 import org.apache.tika.mime.MimeTypes;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+
+import java.util.Optional;
 
 /**
  * 文件拓展名工具类
@@ -27,4 +31,30 @@ public class FileExtUtil {
         }
     }
 
+
+    /**
+     * 将文件拓展名转换为 MimeType
+     *
+     * @param ext 文件拓展名
+     * @return MimeType
+     */
+    public static String ext2mime(String ext) {
+        return ext2mime(ext, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    /**
+     * 将文件拓展名转换为 MimeType
+     *
+     * @param ext 文件拓展名
+     * @return MimeType
+     */
+    public static String ext2mime(String ext, MediaType defaultMediaType) {
+        try {
+            Optional<MediaType> mediaType = MediaTypeFactory.getMediaType(ext);
+            return mediaType.orElse(defaultMediaType).toString();
+        } catch (Exception e) {
+            throw new StrixException("获取文件 MimeType 失败");
+        }
+
+    }
 }
