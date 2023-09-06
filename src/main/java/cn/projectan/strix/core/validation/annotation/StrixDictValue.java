747078a67b24444545aa4f42f59365d3e707541c
0xyk3r <anjiongyi@163.com>
2023-09-06 17:18:04 +0800

    Strix 1.5.6
    Feats
    1. add customer validator `StrixDictValue`.
    
    Fixes
    empty.

---
+++ src/main/java/cn/projectan/strix/core/validation/annotation/StrixDictValue.java
@@ -0,0 +1,29 @@
+package cn.projectan.strix.core.validation.annotation;
+
+import cn.projectan.strix.core.validation.validator.StrixDictValueValidator;
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author 安炯奕
+ * @date 2023/9/6 16:45
+ */
+@Documented
+@Constraint(validatedBy = {StrixDictValueValidator.class})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+public @interface StrixDictValue {
+
+    String message() default "{error.validation.dict}";
+
+    Class<?>[] groups() default {};
+
+    Class<?> dict();
+
+}
