19932627405f62459a0ff4e93d6f0d96eb6155c8
0xyk3r <anjiongyi@163.com>
2023-09-29 18:17:37 +0800

    Strix 1.5.17
    Feats
    1. 增加了工具类 `CompletableUtil` 用于异步的执行多个任务，并等待它们全部执行完成 `allOf` 或任意一个执行完成 `anyOf`。
    
    Fixes
    empty.

--- src/main/java/cn/projectan/strix/controller/system/DebugController.java
+++ src/main/java/cn/projectan/strix/controller/system/DebugController.java
@@ -6,6 +6,7 @@ import cn.projectan.strix.job.PopularityJob;
 import cn.projectan.strix.model.annotation.Anonymous;
 import cn.projectan.strix.model.annotation.IgnoreDataEncryption;
 import cn.projectan.strix.utils.PopularityUtil;
+import cn.projectan.strix.utils.async.CompletableUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +14,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author 安炯奕
@@ -35,25 +35,49 @@ public class DebugController {
     private final PopularityJob popularityJob;
 
     @IgnoreDataEncryption
-    @GetMapping("test")
-    public RetResult<Object> test(HttpServletRequest request) throws JsonProcessingException {
-        popularityUtil.addPopularity("test", "1");
-        popularityUtil.addPopularity("test", "1");
-        popularityUtil.addPopularity("test", "2");
-        popularityUtil.addPopularity("test", "3");
-        popularityUtil.addPopularity("test", "1");
-        popularityUtil.addPopularity("test", "1");
-        popularityUtil.addPopularity("test", "2");
-        popularityUtil.addPopularity("test", "3");
+    @GetMapping("test/{key}")
+    public RetResult<Object> test(@PathVariable String key, HttpServletRequest request) throws JsonProcessingException {
 
-        List<String> list =
-                List.of(
-                        popularityUtil.getPopularity("test", "1"),
-                        popularityUtil.getPopularity("test", "2"),
-                        popularityUtil.getPopularity("test", "3")
-                );
+        CompletableUtil.allOf(
+                () -> {
+                    log.info("test1");
+                    try {
+                        Thread.sleep(5000);
+                        log.info("5000");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                () -> {
+                    log.info("test2");
+                    try {
+                        Thread.sleep(7000);
+                        log.info("7000");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                () -> {
+                    log.info("test3");
+                    try {
+                        Thread.sleep(3000);
+                        log.info("3000");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                () -> {
+                    log.info("test4");
+                    try {
+                        Thread.sleep(9000);
+                        log.info("9000");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
 
-        return RetMarker.makeSuccessRsp(list);
+        return RetMarker.makeSuccessRsp();
     }
 
 }
