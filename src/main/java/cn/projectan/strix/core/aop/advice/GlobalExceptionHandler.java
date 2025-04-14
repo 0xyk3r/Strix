ccdf919552d79be08477990b53f291726f5714ab
0xyk3r <anjiongyi@163.com>
2025-04-14 17:02:42 +0800

    feat: 全局异常拦截器增加参数缺失异常拦截.

--- src/main/java/cn/projectan/strix/core/aop/advice/GlobalExceptionHandler.java
+++ src/main/java/cn/projectan/strix/core/aop/advice/GlobalExceptionHandler.java
@@ -8,12 +8,16 @@ import cn.projectan.strix.core.ret.RetResult;
 import cn.projectan.strix.util.I18nUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -33,62 +37,106 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public RetResult<Object> handleConstraintViolationException(MethodArgumentNotValidException e) {
+    public ResponseEntity<RetResult<Object>> handleConstraintViolationException(MethodArgumentNotValidException e) {
         List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
         String message = allErrors.stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .filter(StringUtils::hasText)
                 .collect(Collectors.joining("、"));
-        return RetBuilder.error(message);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(message));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public RetResult<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    public ResponseEntity<RetResult<Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         log.error(e.getMessage(), e);
-        return RetBuilder.error(I18nUtil.get("error.paramsNotAllow"));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(I18nUtil.get("error.paramsNotAllow")));
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public RetResult<Object> handleNoHandlerFoundException(NoHandlerFoundException e) {
-        return RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound"));
+    public ResponseEntity<RetResult<Object>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound")));
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public RetResult<Object> handleNoResourceFoundException(NoResourceFoundException e) {
-        return RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound"));
+    public ResponseEntity<RetResult<Object>> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound")));
     }
 
     @ExceptionHandler(StrixNoAuthException.class)
-    public RetResult<Object> handleStrixNoAuthException(StrixNoAuthException e) {
-        return RetBuilder.build(RetCode.NOT_LOGIN, I18nUtil.get("error.notLogin"));
+    public ResponseEntity<RetResult<Object>> handleStrixNoAuthException(StrixNoAuthException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.build(RetCode.NOT_LOGIN, I18nUtil.get("error.notLogin")));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public RetResult<Object> handleAccessDeniedException(AccessDeniedException e) {
-        return RetBuilder.build(RetCode.NOT_PERMISSION, I18nUtil.get("error.notPermission"));
+    public ResponseEntity<RetResult<Object>> handleAccessDeniedException(AccessDeniedException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.build(RetCode.NOT_PERMISSION, I18nUtil.get("error.notPermission")));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public RetResult<Object> handleIllegalArgumentException(IllegalArgumentException e) {
-        return RetBuilder.error(e.getMessage());
+    public ResponseEntity<RetResult<Object>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<RetResult<Object>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(I18nUtil.get("error.paramsNotPresent")));
     }
 
     @ExceptionHandler(StrixException.class)
-    public RetResult<Object> handleStrixException(StrixException e) {
-        return RetBuilder.error(e.getMessage());
+    public ResponseEntity<RetResult<Object>> handleStrixException(StrixException e) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public RetResult<Object> handleException(Exception e) {
+    public ResponseEntity<RetResult<Object>> handleException(Exception e) {
         if (e instanceof NoHandlerFoundException) {
-            return RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound"));
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound")));
         } else if (e instanceof NoResourceFoundException) {
-            return RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound"));
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(RetBuilder.build(RetCode.NOT_FOUND, I18nUtil.get("error.apiNotFound")));
         } else if (e instanceof HttpRequestMethodNotSupportedException) {
-            return RetBuilder.build(RetCode.METHOD_ERROR, I18nUtil.get("error.apiMethodUnsupported"));
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                    .body(RetBuilder.build(RetCode.METHOD_ERROR, I18nUtil.get("error.apiMethodUnsupported")));
         }
         log.error(e.getMessage(), e);
-        return RetBuilder.error(I18nUtil.get("error.otherError"));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .body(RetBuilder.error(I18nUtil.get("error.otherError")));
     }
 
 }
