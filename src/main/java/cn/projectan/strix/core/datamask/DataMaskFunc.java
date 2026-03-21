8f4d444850b69627448c92be767f5c9e5968fbc3
0xyk3r <anjiongyi@163.com>
2026-03-21 15:53:34 +0800

    opti: 优化 DataMask 功能组件.

--- src/main/java/cn/projectan/strix/core/datamask/DataMaskFunc.java
+++ src/main/java/cn/projectan/strix/core/datamask/DataMaskFunc.java
@@ -11,49 +11,49 @@ import org.springframework.util.StringUtils;
 public enum DataMaskFunc {
 
     /**
-     * 保留前n位，后面用*代替
+     * 保留前n位，后面用脱敏字符代替
      */
     KEEP_FRONT((data, maskChar, n1, n2) -> {
         if (!StringUtils.hasText(data)) {
             return null;
         }
         if (data.length() <= n1) {
-            return "******";
+            return String.valueOf(maskChar).repeat(6);
         }
-        return data.replaceAll("(?<=\\w{" + n1 + "})\\w", "*");
+        return data.substring(0, n1) + String.valueOf(maskChar).repeat(data.length() - n1);
     }),
     /**
-     * 保留后n位，前面用*代替
+     * 保留后n位，前面用脱敏字符代替
      */
     KEEP_BACK((data, maskChar, n1, n2) -> {
         if (!StringUtils.hasText(data)) {
             return null;
         }
         if (data.length() <= n1) {
-            return "******";
+            return String.valueOf(maskChar).repeat(6);
         }
-        return data.replaceAll(".(?=.{" + n1 + "})", "*");
+        return String.valueOf(maskChar).repeat(data.length() - n1) + data.substring(data.length() - n1);
     }),
     /**
-     * 保留前后各n位，中间用*代替
+     * 保留前n1位和后n2位，中间用脱敏字符代替
      */
     KEEP_SIDE((data, maskChar, n1, n2) -> {
         if (!StringUtils.hasText(data)) {
             return null;
         }
         if (data.length() <= n1 + n2) {
-            return data;
+            return String.valueOf(maskChar).repeat(6);
         }
-        return data.replaceAll("(?<=\\w{" + n1 + "})\\w(?=\\w{" + n2 + "})", "*");
+        return data.substring(0, n1) + String.valueOf(maskChar).repeat(data.length() - n1 - n2) + data.substring(data.length() - n2);
     }),
     /**
-     * 不保留任何位，全部用*代替
+     * 不保留任何位，全部用脱敏字符代替
      */
     KEEP_NONE((data, maskChar, n1, n2) -> {
         if (!StringUtils.hasText(data)) {
             return null;
         }
-        return data.replaceAll("\\w", "*");
+        return String.valueOf(maskChar).repeat(data.length());
     });
 
 
