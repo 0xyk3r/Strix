ccf4fdcb7f0e0273e43d145f5fab89a1852d829a
0xyk3r <anjiongyi@163.com>
2026-03-21 22:02:24 +0800

    fix: 修复线程池硬编码参数.

--- src/main/java/cn/projectan/strix/core/threadpool/CompletableThreadPool.java
+++ src/main/java/cn/projectan/strix/core/threadpool/CompletableThreadPool.java
@@ -5,10 +5,17 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
+ * 全局可完成异步任务线程池
+ *
  * @author ProjectAn
  * @since 2023/9/29 17:55
  */
 public class CompletableThreadPool {
+
+    private static final int CORE_POOL_SIZE = 20;
+    private static final int MAX_POOL_SIZE = 50;
+    private static final int QUEUE_CAPACITY = 1000;
+    private static final int KEEP_ALIVE_SECONDS = 300;
 
     private static volatile ThreadPoolTaskExecutor INSTANCE;
 
@@ -20,10 +27,10 @@ public class CompletableThreadPool {
             synchronized (CompletableThreadPool.class) {
                 if (INSTANCE == null) {
                     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-                    executor.setCorePoolSize(20);
-                    executor.setMaxPoolSize(50);
-                    executor.setQueueCapacity(1000);
-                    executor.setKeepAliveSeconds(300);
+                    executor.setCorePoolSize(CORE_POOL_SIZE);
+                    executor.setMaxPoolSize(MAX_POOL_SIZE);
+                    executor.setQueueCapacity(QUEUE_CAPACITY);
+                    executor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
                     executor.setThreadNamePrefix("strix-completable-executor-");
                     executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
                     executor.initialize();
