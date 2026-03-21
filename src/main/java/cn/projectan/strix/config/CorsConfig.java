28bfc737a6557d84afb6186e1977993e3058443c
0xyk3r <anjiongyi@163.com>
2026-03-21 17:09:07 +0800

    feat: CORS 域名配置外部化.

--- src/main/java/cn/projectan/strix/config/CorsConfig.java
+++ src/main/java/cn/projectan/strix/config/CorsConfig.java
@@ -1,12 +1,13 @@
 package cn.projectan.strix.config;
 
+import cn.projectan.strix.model.properties.system.StrixCorsProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.util.Collections;
 
 /**
  * 跨域配置
@@ -15,12 +16,16 @@ import java.util.Collections;
  * @since 2021/05/02 17:11
  */
 @Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(StrixCorsProperties.class)
 public class CorsConfig {
+
+    private final StrixCorsProperties corsProperties;
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 允许任何来源
-        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        // 从配置文件读取允许的域名列表
+        corsConfiguration.setAllowedOriginPatterns(corsProperties.getAllowedOrigins());
         // 允许任何请求头
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
         // 允许任何方法
---
+++ src/main/java/cn/projectan/strix/model/properties/system/StrixCorsProperties.java
@@ -0,0 +1,25 @@
+package cn.projectan.strix.model.properties.system;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * CORS 跨域配置属性
+ *
+ * @author ProjectAn
+ * @since 2026/3/20
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "strix.cors")
+public class StrixCorsProperties {
+
+    /**
+     * 允许的域名列表，默认允许所有
+     */
+    private List<String> allowedOrigins = List.of("*");
+
+}
